6937209a4d2fbd93c672aabb21df4d4a13f6dde8
tccsantos <109820840+tccsantos@users.noreply.github.com>
2025-04-09 20:28:10 -0300

    Update_4

--- Trabalho2/Campeonato.py
+++ Trabalho2/Campeonato.py
@@ -1,6 +1,7 @@
 from typing import Literal
 
 categorias = Literal["infantil", "juvenil", "adulto"]
+categorias_real = ["infantil", "juvenil", "adulto"]
 
 class Pessoa():
     __idade: int
@@ -18,7 +19,8 @@ class Pessoa():
         if self.__idade < 10 or self.__idade > 60: return False
         if self.__categoria not in categorias: return False
         if self.__categoria == "infantil" and self.__idade > 14: return False
-        if self.__categoria == "juvenil" and (self.__idade < 15 or self.__idade > 17): return False
+        if self.__categoria == "juvenil" and self.__idade < 15: return False
+        if self.__categoria == "juvenil" and self.__idade > 17: return False
         if self.__categoria == "adulto" and self.__idade < 18: return False
         if self.__tempo < 30 or self.__tempo > 180: return False
         return self.__termo
